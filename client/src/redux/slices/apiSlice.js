b71329fbfc827fa7187fefbb63d6f515e5cdb0ce
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-04-28 23:42:11 +0530

    Update apiSlice.js

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
 // const API_URL = "http://localhost:8800/api";
-const API_URL = "https://taskify-full-stack-project-using-mern.onrender.com";
+const API_URL = "http://34.231.243.201:5000";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
